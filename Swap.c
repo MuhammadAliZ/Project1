2b0c63aa5ecf80ae80e7b60cd366f4c99df86caa
MuhammadAliZ <Muhammad.AliZ@outlook.com>
2018-11-20 20:48:43 +0000

    Added the Swapping function

--- Swap.c
+++ Swap.c
@@ -4,12 +4,15 @@ int main(void)
 {
   int x = 28;
   int y = 50;
-  
+
   printf("x is %i\n", x);
   printf("y is %i\n", y);
-  
+
+  int temp = x;
+  x = y;
+  temp = y;
   //Swapping....
-  
+
   printf("x is now %i\n", x);
   printf("y is now %i\n", y);
 }
